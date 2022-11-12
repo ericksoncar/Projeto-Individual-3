b1f4d37d4ed63fd1a3079d7f1a952188aa5d4867
ericksoncar <113147990+ericksoncar@users.noreply.github.com>
2022-11-12 20:16:56 -0300

    commit server

---
+++ server.js
@@ -0,0 +1,13 @@
+//import jsonServer from 'json-server';
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 3200;
+
+server.use(middleware);
+server.use(router);
+
+server.listen(port, ()=> {
+    console.log(`Pegou Bro ${port}`)
+});
